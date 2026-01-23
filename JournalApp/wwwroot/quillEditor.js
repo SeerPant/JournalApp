e45cad9aaa91a558284f4ba393c0718ebd23148f
Seer Pant <spant003@gmail.com>
2026-01-23 16:43:54 +0545

    fixed quill js integration, change layout of cards while viewing list

---
+++ JournalApp/wwwroot/quillEditor.js
@@ -0,0 +1,8 @@
+﻿let quill;
+window.initQuill = (divId) => {
+    quill = new Quill(`#${divId}`, { theme: "snow" });
+};
+window.getQuillHTML = () => quill.root.innerHTML;
+window.setQuillHTML = (htmlContent) => {
+    quill.root.innerHTML = htmlContent;
+};
